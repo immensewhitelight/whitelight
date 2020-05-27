8e582aa98f40096e5e1c2760817b0b01a0631d14
user <user@debian>
2020-05-27 16:22:05 -0400

    17

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
   <!--  <link rel="stylesheet" href="css/app.css"> -->
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Jobs app</title>
+    <title>Ideas app</title>
     
     
 </head>
